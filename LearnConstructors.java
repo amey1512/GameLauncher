8eb874e5a765833e2b2ea562d0371d20540f4741
Amey Mahajan <amey.mahajan@metricstream.com>
2021-04-18 01:40:59 +0530

    updated constructor files

--- LearnConstructors.java
+++ LearnConstructors.java
@@ -2,8 +2,9 @@ class MyConstructor {
 	int sid;
 	String sname;
 	int age; 
+	
 	// Basic Constructor and parameterized constructor its overloading
-	MyConstructor(int i, int a){
+/*	MyConstructor(int i, int a){
 		sid = i;
 		age = a;
 	}
@@ -11,18 +12,32 @@ class MyConstructor {
 		sid = i;
 		age = a;
 		sname = s;
-	}
+	} 
+*/
 	
+	//Copy Constructors
+	MyConstructor(int sid, String sname){
+		this.sid = sid;
+		this.sname = sname;
+	}
+	MyConstructor(MyConstructor myc){
+		this.sid = myc.sid;
+		this.sname = myc.sname;
+	}
 }
 
-
 public class LearnConstructors {
+	
+	static {
+		System.out.println("Checking Static block of Java and its call");
+	}
+	
 	public static void main(String args[]) {
 		
-		MyConstructor myc = new MyConstructor(005, "Amey", 12);
-		System.out.println(myc.sid);
-		System.out.println(myc.age);
-		System.out.println(myc.sname);
-		
+		MyConstructor myc1 = new MyConstructor(5, "Amey");
+		System.out.println(myc1.sid);
+		System.out.println(myc1.sname);
+		MyConstructor myc2 = new MyConstructor(myc1);
+		System.out.println(myc2.sname);
 	}
 }
